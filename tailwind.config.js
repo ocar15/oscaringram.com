517893c5bfcef060b941cd7ee9abbc7ce87c04b4
Oscar Ingram <oscardingram@gmail.com>
2023-07-14 16:25:10 -0700

    Just a whole lot of stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'bisque': "#FFE4C4"
+        'bisque': "#FFE4C4",
+        'oscar-red':"#c00000",
+        'oscar-purple':"#2a0026",
+        'oscar-pink':"#ed7f80"
       },
       backgroundImage: {
         'cubes': "url('/img/cubes.png')"
